0343fecbd18ea6d3651ccba863550ad1bc61ad9d
Pauric Boyle <pauricboylegti@gmail.com>
2016-01-07 15:31:37 +0000

    File Parser working were it reads in from 4grams.txt and stores it in a Hash map

--- src/ie/gmit/sw/FilesParser.java
+++ src/ie/gmit/sw/FilesParser.java
@@ -1,16 +1,34 @@
 package ie.gmit.sw;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class FilesParser {
-
-	public Map<String, Double> parse (String file)
+	
+	String line=null;
+	public Map<String, Double> parse (String file) throws NumberFormatException, IOException 
 	{
 		Map<String, Double> temp = new ConcurrentHashMap<String, Double>();
-		//BufferedReader br = new BufferedReader(new InputStream(""));
+		BufferedReader input = null;
+		try {
+			input = new BufferedReader(new InputStreamReader(new FileInputStream("4grams.txt")));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		while ((line = input.readLine())!= null) {
+			
+			String[] Score = line.split(" ");
+			temp.put(Score[0], Double.parseDouble(Score[1]));	
+				
+			 }//while
+		input.close();
 		return temp;
 	}
 }
---
+++ src/ie/gmit/sw/RailFenceRunner.java
@@ -0,0 +1,11 @@
+package ie.gmit.sw;
+
+import java.io.IOException;
+
+public class RailFenceRunner {
+
+	public static void main(String[] args) throws IOException {
+		
+	}
+
+}
