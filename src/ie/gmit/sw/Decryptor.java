6e49fae791363df988bf8a4820192d7bbb9f708e
Pauric Boyle <pauricboylegti@gmail.com>
2016-01-12 00:01:26 +0000

    Threads working and decrypt now working. Tested with breakpoints and using debug

--- src/ie/gmit/sw/Decryptor.java
+++ src/ie/gmit/sw/Decryptor.java
@@ -7,23 +7,26 @@ public class Decryptor implements Runnable {//producer
 	private BlockingQueue<Resultable> queue;
 	private String cypherText;
 	private int key;
+	private TextScorer tScore;
 	
 	public Decryptor(BlockingQueue<Resultable> queue, String cypherText, int key) {
 		super();
 		this.queue = queue;
 		this.cypherText = cypherText;
 		this.key = key;
+		this.tScore = new TextScorer(QuadGramMap.QGMap);
 	}
 	public void run() {
+		double score;
 		RailFence rf = new RailFence();
 		String plainText = rf.decrypt(cypherText, key);
-		Resultable r = null;
-		r.setKey(key);
-		r.setPlainText(plainText);
+		score = tScore.getScore(plainText);
+		Resultable r = new Result(plainText, key, score);
+		
 		try {
 			queue.put(r);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
+			
 			e.printStackTrace();
 		}
 		
